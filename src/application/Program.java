4c51b648c70a6f04f4b60b0733cebeec13a1fcd4
unknown <o.adrian.rodrigues@gmail.com>
2023-05-10 10:53:52 -0300

    solo try

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,52 @@
 package application;
 
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Contract;
+import model.entities.Installments;
+import model.service.ContractService;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args){
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		
+		System.out.println("Enter the contract data: ");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Date(dd/MM/yyyy): ");
+		LocalDate date = LocalDate.parse(sc.next(), fmt);
+		System.out.print("Contract value: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract contract = new Contract(number, date, totalValue);
+		
+		System.out.print("Enter the number os installments: ");
+		int months = sc.nextInt();
+		
+		ContractService service = new ContractService(null);
+		
+		service.processContract(contract, months);
+		
+		System.out.println("Installments: ");
+		
+		for(Installments installment : contract.getInstallments()) {
+			System.out.print(installment);
+		}
+		sc.close();
+		
+		
+		
+		
 
 	}
 
